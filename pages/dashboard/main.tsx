0fa4e515ed875684a27f46ed7538e826f8a23277
Noah Snodgrass <noahsnod4@gmail.com>
2024-04-24 22:45:35 -0700

    Updates in size. Redraw image wider

--- pages/dashboard/main.tsx
+++ pages/dashboard/main.tsx
@@ -8,9 +8,10 @@ export default function Home() {
     <div className="flex flex-col items-center justify-center min-h-screen pt-50 relative">
       <TwinklingStars numberOfStars={200} />
       <ShootingStar />
-      <div className="flex flex-col items-center justify-center h-[90rem] pt-20"> {/* Added padding-top here */}
-        <img src="/images/main_totoro/totoro.png" alt="Totoro" style={{ zIndex: 10, position: 'relative' }} />
+      <div className="container">
+        <img src="/images/main_totoro/totoro.png" alt="Totoro" className="totoro-img" />
       </div>
+
     </div>
   );
 }
